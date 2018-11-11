b7b043236dc3c1511f2739d6b74447ca53bdde65
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2018-11-10 23:32:49 -0500

    modify bufferWrite to buffer output dynamically until no data is received after 4ms
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <jrzerwick@gmail.com>
    Co-authored-by: Nick Johnson <njohnson7@pm.me>

--- repl/Repl.js
+++ repl/Repl.js
@@ -22,32 +22,47 @@ const Repl = {
     this.process.write(string + '\n');
   },
 
-  bufferWrite(string, ms = 15) {
+  bufferWrite(string, ms = 10) {
     return new Promise((resolve, reject) => {
       let result = '';
+      let dataReceived = false;
 
-      let concatResult = (data) => {
+      const concatResult = (data) => {
         result += data;
+        dataReceived = true;
       };
 
       this.process.write(string + '\n');
-
       this.process.on('data', concatResult);
 
-      setTimeout(() => {
-        resolve(result);
-        this.process.removeListener('data', concatResult);
-      }, ms); // wait for output to buffer
+      new Promise(res => {
+        const id = setInterval(() => {
+          if (dataReceived) {
+            clearInterval(id);
+            res();
+          }
+        }, 1);
+      }).then(() => {
+        let currResult = result;
+
+        const id = setInterval(() => {
+          
+          if (currResult === result) {
+            resolve(result);
+            clearInterval(id);
+            this.process.removeListener('data', concatResult);      
+          } else {
+            currResult = result;
+          }
+        }, 4);
+      });
     });
   },
 
   bufferRead(ms = 400) {
     return new Promise((resolve, reject) => {
       let result = '';
-
-      let concatResult = (data) => {
-        result += data;
-      };
+      const concatResult = (data) => (result += data);
 
       this.process.on('data', concatResult);
 
