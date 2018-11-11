f692c77c8a72b01be1ab1b54430ce4ee250a8b56
Ying Chyi Gooi <25574844+YingCGooi@users.noreply.github.com>
2018-11-11 00:16:57 -0500

    modify bufferWrite to buffer output dynamically until no data is received after 4ms (#8)
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <jrzerwick@gmail.com>
    Co-authored-by: Nick Johnson <njohnson7@pm.me>

--- repl/Repl.js
+++ repl/Repl.js
@@ -27,19 +27,45 @@ const Repl = {
     this.process.write(string + '\n');
   },
 
-  bufferWrite(string, ms = 15) {
+  bufferWrite(string, ms = 10) {
+
     console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string = ${string}, ms = ${ms})]`);
+
     return new Promise((resolve, reject) => {
       let result = '';
+      let dataReceived = false;
 
       const concatResult = (data) => {
         console.log(`${Date().slice(4, 33)} -- [bufferWrite: concatResult(data = ${data})]`);
+
         result += data;
+        dataReceived = true;
       };
 
       this.process.write(string + '\n');
-
       this.process.on('data', concatResult);
+
+      new Promise(res => {
+        const id = setInterval(() => {
+          if (dataReceived) {
+            clearInterval(id);
+            res();
+          }
+        }, 1);
+      }).then(() => {
+        let currResult = result;
+
+        const id = setInterval(() => {
+          
+          if (currResult === result) {
+            resolve(result);
+            clearInterval(id);
+            this.process.removeListener('data', concatResult);      
+          } else {
+            currResult = result;
+          }
+        }, 4);
+      });
 
       setTimeout(() => {
         console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): setTimeout()]`);
@@ -53,12 +79,13 @@ const Repl = {
     console.log(`${Date().slice(4, 33)} -- [Repl.bufferRead(ms = ${ms})]`);
     return new Promise((resolve, reject) => {
       let result = '';
+      const concatResult = (data) => (result += data);
 
       const concatResult = (data) => {
         console.log(`${Date().slice(4, 33)} -- [bufferRead: concatResult(data = ${data})]`);
         result += data;
       };
-
+      
       this.process.on('data', concatResult);
 
       setTimeout(() => {
