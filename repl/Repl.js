134bfd41cb9c3dc46bc764b7b78a7a1ef6eda695
Nick Johnson <njohnson7@pm.me>
2018-11-11 01:43:56 -0800

    added more error logs, more console.logs, and nil guards to Repl.js
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius Zerwick <jrzerwick@gmail.com>
    Co-authored-by: Nick Johnson <njohnson7@pm.me>

--- repl/Repl.js
+++ repl/Repl.js
@@ -28,8 +28,9 @@ const Repl = {
     this.process.write(string + '\n');
   },
 
+  // TODO: Remove repetitive code in bufferWrite and bufferRead
+  // TODO: Improve clarity of promises in bufferWrite and bufferRead
   bufferWrite(string, ms = 10) {
-
     console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string = ${string}, ms = ${ms})]`);
 
     return new Promise((resolve, reject) => {
@@ -46,32 +47,48 @@ const Repl = {
       this.process.write(string + '\n');
       this.process.on('data', concatResult);
 
-      new Promise(res => {
+      new Promise((res) => {
+        // TODO: how does this interval get cleared if no data is received?
         const id = setInterval(() => {
           if (dataReceived) {
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)) -- if (dataReceived)] -- before 'clearInterval(id); res()'`);
             clearInterval(id);
             res();
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)) -- if (dataReceived)] -- after 'clearInterval(id); res()'`);
           }
         }, 1);
       }).then(() => {
         let currResult = result;
 
         const id = setInterval(() => {
-          
+        // TODO: how does this interval get cleared if currResult never equals result?
           if (currResult === result) {
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)).then -- if currResult === result] -- before 'resolve(result)'`);
             resolve(result);
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)).then -- if currResult === result] -- after 'resolve(result)', before 'clearInterval(id)'`);
             clearInterval(id);
-            this.process.removeListener('data', concatResult);      
+
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)).then -- if currResult === result] -- after 'clearInterval(id)', before 'this.process.removeListener("data", concatResult)'`);
+            // added nil guard:
+            this.process && this.process.removeListener('data', concatResult);      
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)).then -- if currResult === result] -- after 'this.process.removeListener("data", concatResult)'`);
           } else {
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)).then -- else currResult !== result] -- before 'currResult = result'`);
             currResult = result;
+            console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): new Promise(fn(res)).then -- else currResult !== result] -- after 'currResult = result'`);
           }
         }, 4);
       });
 
       setTimeout(() => {
-        console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): setTimeout()]`);
+        console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): setTimeout()] -- before 'resolve(result)'`);
         resolve(result);
-        this.process.removeListener('data', concatResult);
+        console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): setTimeout()] -- after 'resolve(result)'`);
+
+        console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): setTimeout()] -- before 'removeListener("data", concatResult)'`);
+        // added nil guard:
+        this.process && this.process.removeListener('data', concatResult);
+        console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): setTimeout()] -- after 'removeListener("data", concatResult)'`);
       }, ms); // wait for output to buffer
     });
   },
@@ -92,7 +109,8 @@ const Repl = {
       setTimeout(() => {
         console.log(`${Date().slice(4, 33)} -- [Repl.bufferWrite(string, ms): setTimeout()]`);
         resolve(result);
-        this.process.removeListener('data', concatResult);
+        // added nil guard:
+        this.process && this.process.removeListener('data', concatResult);
       }, ms); // wait for output to buffer
     });
   },
