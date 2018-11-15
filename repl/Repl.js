47079346916c428cd21a3d8a3c28d1963cb7a0f5
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2018-11-15 18:05:17 -0500

    fix prompts
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <jrzerwick@gmail.com>
    Co-authored-by: Nick Johnson <njohnson7@pm.me>

--- repl/Repl.js
+++ repl/Repl.js
@@ -36,46 +36,15 @@ const Repl = {
     });
   },
 
-  // TODO: Improve clarity of promises in bufferWrite and bufferRead.
-  // TODO: Add reject function and/or catch clauses to all promises and 'thens'
-  //       in this code and throughout project, so that all errors will be
-  //       handled and/or logged.
-  // bufferWrite(string, bufferInterval = 5, write = true) {
-  //   let result = '';
-  //   const concatResult = (data) => result += data;
-  //   const isDataReceived = () => result !== '';
-
-  //   if (write) this.process.write(string + '\n');
-  //   this.process.on('data', concatResult);
-
-  //   return new Promise(async (resolve, _reject) => {
-  //     await this.untilCondIsMet(isDataReceived);
-
-  //     let currResult = result;
-  //     const noNewDataReceived = () => currResult === result;
-
-  //     const intervalId = setInterval(() => {
-  //       if (currResult !== result) return currResult = result;
-
-  //       clearInterval(intervalId);
-  //       this.removeListener('data', concatResult);
-  //       resolve(result);
-  //     }, bufferInterval);
-  //   });
-  // },
-
   bufferRead(bufferInterval) {
     return this.bufferWrite('', bufferInterval, write = false);
   },
 
   kill() {
+    this.process && this.process.removeAllListeners('data');
     if (this.process) this.process.kill();
     this.process = null;
     this.language = null;
-  },
-
-  removeListener(event, func) {
-    this.process && this.process.removeListener(event, func);
   },
 
   // TODO: delete this if not used anywhere
