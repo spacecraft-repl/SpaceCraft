da4869f6732c46d48f0a370f89a36ee8903978ac
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2018-11-16 11:21:00 -0500

    syncSelf defaulted to undefined - property won't be included unless it is truthy
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <jrzerwick@gmail.com>
    Co-authored-by: Nick Johnson <njohnson7@pm.me>

--- src/client.js
+++ src/client.js
@@ -79,7 +79,7 @@ const ClientRepl = {
   },
 
   // Emit 'lineChanged` event to server --> server broadcasts 'syncLine' to clients.
-  emitLineChanged({ syncSelf = false } = {}) {
+  emitLineChanged({ syncSelf = undefined } = {}) {
     socket.emit('lineChanged', { line: state.line, syncSelf });
   },
 
