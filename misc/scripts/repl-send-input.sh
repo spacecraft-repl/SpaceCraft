cb0e115020a949b81cb6a75f4e447939a5ac7ad3
Nick Johnson <njohnson7@pm.me>
2018-11-17 05:50:14 -0800

    Revert script changes
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius Zerwick <jrzerwick@gmail.com>
    Co-authored-by: Nick Johnson <njohnson7@pm.me>

--- misc/scripts/repl-send-input.sh
+++ misc/scripts/repl-send-input.sh
@@ -16,8 +16,4 @@ while true; do
       --clearmodifiers \
       Return 0 Return 1 2 Return 3 4 5 Return 6 7 8 9 Return \
     sleep 0.175
-
-  while true; do
-    xdotool search --onlyvisible --name "nvim" windowactivate sleep 300
-  done
 done
