f57023233985995bcdca77de03781d4997f93a55
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-02-06 09:00:23 -0500

    package json to ignore test.js when starting standard

--- repl/test.js
+++ repl/test.js
@@ -1,8 +1,8 @@
-const Repl = require('./Repl.js');
+const Repl = require('./Repl.js')
 
 const assert = (expected, actual) => {
   if (expected === actual) {
-    console.log('✓ Test passed');
+    console.log('✓ Test passed')
   } else {
     throw new Error(`\nexpected: ${expected}\nactual: ${actual}`)
   }
@@ -10,115 +10,113 @@ const assert = (expected, actual) => {
 
 const assertIncludes = (expected, actual) => {
   if (actual.includes(expected)) {
-    console.log('✓ Test passed');
+    console.log('✓ Test passed')
   } else {
     throw new Error(`\nexpected: ${expected}\nactual: ${actual}`)
   }
 }
 
-async function test() {
-  Repl.init('ruby');
-  let result = '';
+async function test () {
+  Repl.init('ruby')
+  let result = ''
 
   Repl.process.on('data', output => {
-    result += output;
+    result += output
   })
 
   const writeRubyPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      Repl.write('[1,2,3].map(&:to_s)');
-      resolve();
-    }, 500);
+      Repl.write('[1,2,3].map(&:to_s)')
+      resolve()
+    }, 500)
   })
 
-  await writeRubyPromise;
+  await writeRubyPromise
 
   const assertRuby = new Promise((resolve, reject) => {
     setTimeout(() => {
       assert(`irb(main):001:0> [1,2,3].map(&:to_s)\r
 => ["1", "2", "3"]\r
-irb(main):002:0> `, result);
-      resolve();
-    }, 200);
+irb(main):002:0> `, result)
+      resolve()
+    }, 200)
   })
 
-  await assertRuby;
+  await assertRuby
 
   // JavaScript
   const initJavaScriptPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      Repl.kill();
-      Repl.init('javascript');
-      resolve();
-    }, 100);    
-  });
+      Repl.kill()
+      Repl.init('javascript')
+      resolve()
+    }, 100)
+  })
 
-  await initJavaScriptPromise;
+  await initJavaScriptPromise
 
-  result = '';
+  result = ''
 
   Repl.process.on('data', output => {
-    result += output;
+    result += output
   })
 
   const writeJavaScriptPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      Repl.write('[1,2,3].map(String)');
-      resolve();
-    }, 500);
+      Repl.write('[1,2,3].map(String)')
+      resolve()
+    }, 500)
   })
 
-  await writeJavaScriptPromise;
+  await writeJavaScriptPromise
 
   const assertJavaScript = new Promise((resolve, reject) => {
     setTimeout(() => {
       assert(`\u001b[1G\u001b[0J> \u001b[3G[1,2,3].map(String)\r\r
 [ \u001b[32m\'1\'\u001b[39m, \u001b[32m\'2\'\u001b[39m, \u001b[32m\'3\'\u001b[39m ]\r
-\u001b[1G\u001b[0J> \u001b[3G`, result);
-      resolve();
-    }, 200);
+\u001b[1G\u001b[0J> \u001b[3G`, result)
+      resolve()
+    }, 200)
   })
 
-  await assertJavaScript;
-
-
+  await assertJavaScript
 
   // Python
   const initPythonPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      Repl.kill();
-      Repl.init('python');
-      resolve();
-    }, 100);    
-  });
+      Repl.kill()
+      Repl.init('python')
+      resolve()
+    }, 100)
+  })
 
-  await initPythonPromise;
+  await initPythonPromise
 
-  result = '';
+  result = ''
 
   Repl.process.on('data', output => {
-    result += output;
+    result += output
   })
 
   const writePythonPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      Repl.write('[1,2,3]');
-      resolve();
-    }, 500);
+      Repl.write('[1,2,3]')
+      resolve()
+    }, 500)
   })
 
-  await writePythonPromise;
+  await writePythonPromise
 
   const assertPython = new Promise((resolve, reject) => {
     setTimeout(() => {
-      assertIncludes('>>> [1,2,3]\r\n[1, 2, 3]\r\n>>> ', result);
-      resolve();
-    }, 200);
+      assertIncludes('>>> [1,2,3]\r\n[1, 2, 3]\r\n>>> ', result)
+      resolve()
+    }, 200)
   })
 
-  await assertPython;
+  await assertPython
 
-  Repl.kill();
+  Repl.kill()
 }
 
-test();
+test()
